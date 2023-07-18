a2071c86ebfcec8a70e633485ffcbac402b6126d
0xmove <0xmovebuilder@gmail.com>
2023-07-18 15:56:35 +0800

    add create faucetclient by network

--- lib/faucet_client.dart
+++ lib/faucet_client.dart
@@ -1,5 +1,6 @@
 
 import 'package:aptos/aptos.dart';
+import 'package:aptos/constants.dart';
 import 'package:aptos/http/http.dart';
 
 class FaucetClient {
@@ -16,8 +17,25 @@ class FaucetClient {
     return FaucetClient._(endpoint, clientEndpoint: clientEndpoint, enableDebugLog: enableDebugLog);
   }
 
-  factory FaucetClient.fromClient(String endpoint, AptosClient client, {bool enableDebugLog = false}) {
-    return FaucetClient._(endpoint, client: client, enableDebugLog: enableDebugLog);
+  factory FaucetClient.fromClient(String endpoint, AptosClient client) {
+    return FaucetClient._(endpoint, client: client, enableDebugLog: client.enableDebugLog);
+  }
+
+  factory FaucetClient.fromNetwork(Network network, {bool enableDebugLog = false}) {
+    String endpoint;
+    String clientEndpoint;
+    switch (network) {
+      case Network.testnet:
+        endpoint = Constants.faucetTestAPI;
+        clientEndpoint = Constants.testnetAPI;
+        break;
+      case Network.devnet:
+        endpoint = Constants.faucetDevAPI;
+        clientEndpoint = Constants.devnetAPI;
+      default:
+        throw ArgumentError("Unsupport Network $network");
+    }
+    return FaucetClient._(endpoint, clientEndpoint: clientEndpoint, enableDebugLog: enableDebugLog);
   }
 
   Future<List<String>> fundAccount(String address, String amount, {int timeoutSecs = 20}) async {
--- test/faucet_client_test.dart
+++ test/faucet_client_test.dart
@@ -1,4 +1,5 @@
 
+import 'package:aptos/aptos.dart';
 import 'package:aptos/constants.dart';
 import 'package:aptos/faucet_client.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,7 +7,30 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
 
   test('faucet fund account', () async {
-    final client = FaucetClient.fromEndpoint(Constants.faucetTestAPI, Constants.testnetAPI);
+    final client = FaucetClient.fromEndpoint(
+      Constants.faucetTestAPI, 
+      Constants.testnetAPI,
+      enableDebugLog: true
+    );
+    final result = await client.fundAccount(
+      '0x978c213990c4833df71548df7ce49d54c759d6b6d932de22b24d56060b7af2aa', 
+      "100000000"
+    );
+    expect(result.isNotEmpty, true);
+  });
+
+  test('faucet fund account from client', () async {
+    final aptosClient = AptosClient(Constants.testnetAPI, enableDebugLog: true);
+    final client = FaucetClient.fromClient(Constants.faucetTestAPI, aptosClient);
+    final result = await client.fundAccount(
+      '0x978c213990c4833df71548df7ce49d54c759d6b6d932de22b24d56060b7af2aa', 
+      "100000000"
+    );
+    expect(result.isNotEmpty, true);
+  });
+
+  test('faucet fund account from network', () async {
+    final client = FaucetClient.fromNetwork(Network.testnet, enableDebugLog: true);
     final result = await client.fundAccount(
       '0x978c213990c4833df71548df7ce49d54c759d6b6d932de22b24d56060b7af2aa', 
       "100000000"
